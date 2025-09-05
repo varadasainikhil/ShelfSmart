2883c0c705ed2ba5a54a556eb32f5cc0672113b1
varadasainikhil <varadasainikhil@gmail.com>
2025-09-04 22:07:44 -0400

    working icloud integration

---
+++ ShelfSmart/TODO.swift
@@ -0,0 +1,12 @@
+//
+//  TODO.swift
+//  ShelfSmart
+//
+//  Created by Sai Nikhil Varada on 9/4/25.
+//
+
+import Foundation
+
+// TODO: Important - Swift data works on device but persists across multiple accounts on the same device.
+// That is if i use an account to add products and i log out and then log in with another account - i can still see the same products. I need to find a way to make the products stick to a user id.
+
