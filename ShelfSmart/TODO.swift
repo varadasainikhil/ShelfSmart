35298ef9b5246dd1604ba88a166317c2e529c8dd
varadasainikhil <varadasainikhil@gmail.com>
2025-09-25 00:18:05 -0400

    updated todos

--- ShelfSmart/TODO.swift
+++ ShelfSmart/TODO.swift
@@ -7,16 +7,24 @@
 
 import Foundation
 
-// TODO: 2a. Add a email verification through a code or a link for new users.
-// TODO: 3. Add a one parameter at a time sign up process - User first enters email, clicks next, enters password and confirms it.
-// TODO: 4. Try to add a method to share recipes
-// TODO: 5. Make the UI uniform : Button with glass effect.
+// You are in the MAIN branch
+// TODO: First implement the core functionality of the app, then work on the bugs.
 // TODO: 6. Adjust the app to look good in the dark mode.
 // TODO: 7. Implement Push Notifications, to remind users that their food is expiring in 3 days.
 // TODO: 8. If the product is not marked used and is expired, then remind the user to throw away the product.
+
+
+
 // TODO: 9. Add a settings menu in the profile section. Allow the user to choose which time she wants her notifications to arrive.
 // TODO: 10. Add a setting menu to ask the user if the wants notifications with sound or not.
 // TODO: 11. Add a feature that allows the user to remind her about the product on the day she chooses - the date should not exceed the date of expiry.
+
+// TODO: 3. Add a one parameter at a time sign up process - User first enters email, clicks next, enters password and confirms it.
+// TODO: 4. Try to add a method to share recipes
+// TODO: 5. Make the UI uniform : Button with glass effect.
+
+
+
 
 
 // Later stages
